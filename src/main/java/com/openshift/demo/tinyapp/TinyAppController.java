82717aa86d5fe42d1bfe5883ce8c132db3ac38b4
Siamak <ssadeghi@redhat.com>
2018-10-22 18:42:05 +0200

    removed unused imports

--- src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
+++ src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
@@ -1,7 +1,5 @@
 package com.openshift.demo.tinyapp;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
