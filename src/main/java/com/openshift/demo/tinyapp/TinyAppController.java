7a0b9ab326422691dd47d34599b7d28db23255f9
Siamak <ssadeghi@redhat.com>
2018-10-22 17:07:10 +0200

    color-code versions

--- src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
+++ src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
@@ -3,14 +3,16 @@ package com.openshift.demo.tinyapp;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TinyAppController {
   public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_MAGNETA = "\u001b[35m";
-  private static final String TEMPLATE = "[ %tT ] TinyApp pod=%s status=OK version=" + ANSI_MAGNETA + " %s " + ANSI_RESET + "\n";
+  public static final String[] ANSI_COLORS = {"\u001b[35m", "\u001B[31m", "\u001B[32m", "\u001B[33m", "\u001B[36m"};
+
+  private static final String TEMPLATE = "[ %tT ] TinyApp pod=%s status=OK version=%s\n";
 
   @Value("${pod.name}")
   private String podName;
@@ -18,8 +20,13 @@ public class TinyAppController {
   @Value("${version}")
   private String appVersion;
 
-  @RequestMapping("/api/status")
-  public String status() {
-      return String.format(TEMPLATE, new Date(), podName, appVersion);
+  @RequestMapping("/version")
+  public String status(@RequestHeader(value="User-Agent") String userAgent) {
+      String version = userAgent.contains("curl") ? color(appVersion) : appVersion;
+      return String.format(TEMPLATE, new Date(), podName, version);
+  }
+
+  private String color(String appVersion) {
+    return ANSI_COLORS[appVersion.hashCode() % ANSI_COLORS.length] + appVersion + ANSI_RESET;
   }
 }
