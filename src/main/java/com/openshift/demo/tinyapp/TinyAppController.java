f21ad7a6f4c1050f3e10fdbedbdba2bd9fd1c758
Siamak <ssadeghi@redhat.com>
2018-10-22 16:39:09 +0200

    updated openshift template

--- src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
+++ src/main/java/com/openshift/demo/tinyapp/TinyAppController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TinyAppController {
   public static final String ANSI_RESET = "\u001B[0m";
   public static final String ANSI_MAGNETA = "\u001b[35m";
-  private static final String TEMPLATE = "[ %tT ] TinyApp pod=%s status=OK version=" + ANSI_MAGNETA + "%s" + ANSI_RESET + "\n";
+  private static final String TEMPLATE = "[ %tT ] TinyApp pod=%s status=OK version=" + ANSI_MAGNETA + " %s " + ANSI_RESET + "\n";
 
   @Value("${pod.name}")
   private String podName;
